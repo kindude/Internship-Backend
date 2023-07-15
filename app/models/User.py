0c206da4fe3043497eef43d904a3813d54ecf026
kindude <yehor.dudnik@gmail.com>
2023-07-15 13:58:58 +0100

    Added more fields to User's model Edited env.py Empty migrations fixed

--- app/models/User.py
+++ app/models/User.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ARRAY
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -11,6 +11,11 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
+    city = Column(String(20), nullable=False)
+    country = Column(String(20), nullable=False)
+    phone = Column(String(13), nullable=False)
+    status = Column(Boolean, nullable=False)
+    roles = Column(ARRAY(String))
 
     def __repr__(self):
         return f"<User(id={self.id}, username={self.username}, email={self.email})>"
