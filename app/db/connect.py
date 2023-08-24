f4942d75e2499a2ea9e78eee74e89c2c7b23e7b3
Yehor Dudnik <yehor.dudnik@gmail.com>
2023-08-24 13:50:54 +0100

    from env -- duplicate code

--- app/db/connect.py
+++ app/db/connect.py
@@ -17,13 +17,3 @@ async def connect_Postgres():
     engine = create_async_engine(database_url, future=True, echo=True)
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     return async_session()
-
-
-@asynccontextmanager
-async def connect_Redis():
-    try:
-        redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
-        yield redis_client
-    except Exception as e:
-        print(f"An error occurred while connecting to Redis: {e}")
-
--- app/repositories/redis_repository.py
+++ app/repositories/redis_repository.py
@@ -1,8 +1,6 @@
 from datetime import timedelta
 
-import redis
-
-from db.connect import connect_Redis
+from db.get_db import get_redis
 
 
 class RedisRepository:
@@ -10,8 +8,8 @@ class RedisRepository:
         self.redis_client = None
 
     async def connect(self):
-        self.redis_client = redis.Redis(host='redis', port=6379, decode_responses=True)
-        print(f"redis connected")
+        self.redis_client = get_redis()
+
 
     async def disconnect(self):
         if self.redis_client:
@@ -19,8 +17,7 @@ class RedisRepository:
 
     async def save_quiz(self, quiz_data: dict):
         await self.connect()
-        print(quiz_data['user_id'])
-        redis_key = f"quiz_result:{quiz_data['user_id']}:{quiz_data['quiz_id']}:{quiz_data['time']}"
+        redis_key = f"quiz_result:{quiz_data['user_id']}:{quiz_data['quiz_id']}"
         self.redis_client.hmset(redis_key, quiz_data)
         expiration_time_seconds = int(timedelta(hours=48).total_seconds())
         self.redis_client.expire(redis_key, expiration_time_seconds)
